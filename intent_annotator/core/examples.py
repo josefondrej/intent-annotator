d8e6cb8577fa419ae3c199c7e25ec41f0b5db225
Josef-Ondrej <josef.ondrej@ibm.com>
2020-04-01 13:07:56 +0200

    Add hacky line reformat to support the agreed format of input examples

--- intent_annotator/core/examples.py
+++ intent_annotator/core/examples.py
@@ -13,10 +13,16 @@ class Examples(File):
     def examples(self) -> List[str]:
         return self.json_dict
 
+    def _hacky_line_reformat(self, line: str) -> str:
+        line = line.strip()
+        if line.startswith("["):
+            line = line[1:]
+        return line[1:-2]
+
     def load_from_file_storage(self, file_storage: FileStorage):
         self._file_name = file_storage.filename
         file_storage.save(self.file_path)
 
         with open(self.file_path) as file:
             self._json_dict = file.readlines()
-            self._json_dict = [line.strip() for line in self._json_dict]
+            self._json_dict = [self._hacky_line_reformat(line) for line in self._json_dict]
