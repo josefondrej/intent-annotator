39595c6396c1c490c13076ee661ee7d14169a360
Josef-Ondrej <josef.ondrej@ibm.com>
2020-04-01 12:57:58 +0200

    Handle invalid json on input

--- intent_annotator/core/examples.py
+++ intent_annotator/core/examples.py
@@ -1,5 +1,7 @@
 from typing import List
 
+from werkzeug.datastructures import FileStorage
+
 from intent_annotator.core.file import File
 
 
@@ -10,3 +12,10 @@ class Examples(File):
     @property
     def examples(self) -> List[str]:
         return self.json_dict
+
+    def load_from_file_storage(self, file_storage: FileStorage):
+        self._file_name = file_storage.filename
+        file_storage.save(self.file_path)
+
+        with open(self.file_path) as file:
+            self._json_dict = file.readlines()
--- intent_annotator/web/app.py
+++ intent_annotator/web/app.py
@@ -58,4 +58,4 @@ def annotate():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, )
